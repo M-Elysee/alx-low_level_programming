1a73513daca0cbf375fd8eee1db2851555287b79
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-03-16 18:34:20 +0000

    4-puts.c and 5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+*main - entry point
+*Return: 0 on success
+*/
+
+int main(void)
+{
+	char m[] = "Programming is like building a multilingual puzzle";
+
+	puts(m);
+	return (0);
+}
