a46be95a2f5daa2be0f77a10950dda3a4d77c400
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-03-17 13:49:49 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main - entry point
+*Return:0 on success
+*/
+int main(void)
+{
+	char c;
+	char k = '\n';
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(tolower(c));
+		putchar(k);
+	}
+	return (0);
+}
