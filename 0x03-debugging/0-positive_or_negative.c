1a6f0e1743df8471fde5cf1f7f01f060941938df
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-03-22 14:48:36 +0000

    0-main.c,main.h

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+/**
+*main - entry
+*@i:the character to be tested
+*Return:0 (success)
+*/
+void positive_or_negative(int n)
+{
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+}
