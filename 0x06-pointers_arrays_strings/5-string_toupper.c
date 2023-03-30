809e026f58010bfcb6d328bb7794835266571899
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-03-30 16:10:56 +0000

    5-string_toupper.c debug-1

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,11 +2,12 @@
 /**
 *string_toupper - turns a string into upper case
 *@n: points to the string
-*return: the pointer to the string in upper case
+*Return: the pointer to the string in upper case
 */
 char *string_toupper(char *n)
 {
 	int i;
+
 	for (i = 0; *(n + i) != 0; i++)
 	{
 		if (*(n + i) >= 97 && *(n + i) <= 122)
