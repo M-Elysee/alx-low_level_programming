0b7a791309043169d51ea9bdb4151c54a53d5418
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-04-03 10:59:23 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*_strstr - find the first occurence of a substring into a string
+*@haystack: points to the string
+*@needle: points to the substring
+*Return: from the first occurence of the substring if any
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, k, m;
+
+	for (i = 0; *(haystack); i++)
+	{
+		m = 0;
+		for (k = 0; *(needle + k); k++)
+		{
+			if (*(haystack + i + k) != *(needle + k))
+			{
+				m = 1;
+				break;
+			}
+		}
+		if (m == 0)
+		{
+			return (haystack + i);
+		}
+	}
+	return ('\0');
+}
+
