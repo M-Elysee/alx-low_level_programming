8f61e782e9b0dbdc09698e6660ef817715ec01d3
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-04-10 16:51:12 +0000

    4-add.c- debugged

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
-* main - multiply two argumments
+* main - add arguments
 * @argc: reprents the number of arguments
 * @argv: points to the array of command line arguments
 * Return: 0 on success or 1 on error
 */
 int main(int argc, char *argv[])
 {
-	int i, s = 0, m;
+	int i, m, d = 0;
+	unsigned int j, s = 0;
 
 	for (i = 1; i < argc || argc == 1; i++)
 	{
@@ -18,7 +20,12 @@ int main(int argc, char *argv[])
 			printf("0\n");
 			return (0);
 		}
-		else if (atoi(argv[i]) == 0)
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+				d++;
+		}
+		if (d > 0)
 		{
 			printf("Error\n");
 			return (1);
