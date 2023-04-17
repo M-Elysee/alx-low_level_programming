d9bb1b77fa1354f57dd732ea07def8f830bb2a1f
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-04-17 08:41:57 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,5 @@
+#ifndef 1_PI_h
+#define 1_PI_h
+
+#define PI 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef 3_FUNCTION_LIKE_MACO_H
+#define 3_FUNCTION_LIKE_MACO_H
+
+#define ABS(x) ((x) > (0) ? (x) : (-x))
+#endif
