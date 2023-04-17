23d75e289af939bcce3bd943994c4159cbf74608
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-04-17 20:34:28 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,20 +19,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 	{
-		free(d);
 		return (NULL);
 	}
 	d->name = malloc(sizeof(char) * (i + 1));
 	if (d->name == NULL)
 	{
-		free(d->name);
 		free(d);
 		return (NULL);
 	}
 	d->owner = malloc(sizeof(char) * (j + 1));
 	if (d->owner == NULL)
 	{
-		free(d->owner);
 		free(d->name);
 		free(d);
 		return (NULL);
