39c3b7290a3cf01f8ffd8f2f5f30674c42f6e1e0
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-04-19 18:12:48 +0000

    0x0F. C - Function pointers-2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,10 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; size > 0 && i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]))
-			return (i);
+		for (i = 0; size > 0 && i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
 	}
 	return (-1);
 }
