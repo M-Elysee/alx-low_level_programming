a79a952c1aeebb08bffc3406649a448ab7ba7b00
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-05-02 16:57:28 +0000

    9-insert_nodeint.c-debug2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
 * insert_nodeint_at_index - it inserts a node at a given position
-* @head: points to a pointer to the head of the linked list 
+* @head: points to a pointer to the head of the linked list
 * @idx: is the position of insertion
 * @n: is the data to be contained
 * Return: the address of the new node or NULL on error
@@ -9,37 +9,29 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *temp, *tp, *t, *tmp, *k;
+	listint_t *temp, *tp = *head;
 
 	temp = malloc(sizeof(listint_t));
-	if (temp == NULL)
+	if (temp == NULL || *head == NULL)
 		return (NULL);
 	temp->n = n;
-	if (*head == NULL)
-	{
-		temp->next = NULL;
-		*head = temp;
-		return (temp);
-	}
-	tmp = *head;
-	tp = *head;
-	for (i = 0; tmp->next; i++)
-		tmp = tmp->next;
-	if (idx > i)
-		return (NULL);
+	temp->next = NULL;
 	if (idx == 0)
 	{
 		temp->next = *head;
 		*head = temp;
 		return (temp);
 	}
-	for (i = 0; idx != 0 && i < idx; i++)
+	for (i = 0; tp && i < idx; i++)
 	{
-		k = tp;
-		tp = tp->next;
+		if (i == idx - 1)
+		{
+			temp->next = tp->next;
+			tp->next = temp;
+			return (temp);
+		}
+		else
+			tp = tp->next;
 	}
-	t = tp->next;
-	k->next = temp;
-	temp->next = t;
-	return (temp);
+	return (NULL);
 }
