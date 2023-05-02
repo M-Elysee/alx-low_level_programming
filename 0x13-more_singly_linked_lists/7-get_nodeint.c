d31e1a824908a656af972975d046a299839cc558
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-05-02 15:40:24 +0000

    7-get_nodeint.c-debug

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *temp = head;
+	listint_t *temp = head, *tmp = head;
 
 	for (i = 0; head != NULL && tmp->next; i++)
 		tmp = tmp->next;
