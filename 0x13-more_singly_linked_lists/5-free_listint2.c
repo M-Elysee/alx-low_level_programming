c10a50bc208e73f96865a51fab661eb584691b17
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-05-02 15:35:04 +0000

    5-free_listint2.c-debug

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,19 @@
- #include "lists.h"
+#include "lists.h"
 /**
 * free_listint2 - frees the memory allocated to a list*
 * @head: pointes to the list
 */
 void free_listint2(listint_t **head)
 {
-	if ((*head) != NULL)
+	listint_t *t;
+
+	if (!head)
+		return;
+	while (t)
 	{
-		if ((*head)->next == NULL)
-		{
-			free(*head);
-			return;
-		}
-		free_listint2(&((*head)->next));
+		t = (*head)->next;
 		free(*head);
-		*head = NULL;
+		*head = t;
 	}
+	*head = NULL;
 }
