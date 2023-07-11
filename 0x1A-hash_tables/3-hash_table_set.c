b18b8ecedb00f630c49d38475887c08dc76deb3a
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-07-11 19:30:37 +0000

    3-hash_table_set.c-debug-3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	head = malloc(sizeof(hash_node_t));
-	if (head== NULL)
+	if (head == NULL)
 	{
 		free(val);
 		return (0);
