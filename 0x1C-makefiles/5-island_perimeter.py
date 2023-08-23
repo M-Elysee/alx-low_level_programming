1971af4880c44eb5f17606f1b1fbdf24be448ca8
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-08-23 13:06:32 +0000

    0x1C. C - Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# a function that returns the perimeter of the island described
+
+def island_perimeter(grid):
+    """*find and return the perimiter of the island*"""
+    w = len(grid[0])
+    h = len(grid)
+    k = 0
+    p = 0
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                p += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    k += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    k += 1
+    return (p * 4) - (k * 2)
